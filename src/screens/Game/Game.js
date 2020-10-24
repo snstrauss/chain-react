c2e8992c6e1052b2ff0f3e289943fbb0d4faa2dd
snstrauss <zahavi.guy.85@gmail.com>
2020-10-24 19:25:58 +0300

    css transition time now comes from game logic. the <Game> component defines both the logical step duration, as well as the stylistic one.

--- src/screens/Game/Game.js
+++ src/screens/Game/Game.js
@@ -5,6 +5,7 @@ import { GameContext } from '../../context/GameProvider';
 import S from './Game.module.scss';
 
 let chainInterval;
+const STEP_DURATION = 500;
 export default function Game() {
 
     const { state: { isRunning, cellsToTurn, phase, userInitiated }, methods: gameActions } = useContext(GameContext);
@@ -18,15 +19,19 @@ export default function Game() {
         if (cellsToTurn.length) {
             chainInterval = setTimeout(() => {
                 gameActions.rotateCells();
-            }, userInitiated ? 0 : 1100)
+            }, userInitiated ? 0 : STEP_DURATION)
         } else {
             clearTimeout(chainInterval);
             gameActions.stopChain();
         }
     }, [phase, isRunning, cellsToTurn.length, gameActions])
 
+    const customStyle = {
+        "--step-duration": STEP_DURATION + 'ms'
+    }
+
     return (
-        <div className={S.container}>
+        <div className={S.container} style={customStyle}>
             <Header />
             <Board />
         </div>
